16afb4db346ace99d438699691d504e6ba442396
Exesilvestre <exesilvestre@gmail.com>
2024-11-08 08:22:48 -0300

    deleting import

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from db_management.db_manager import DatabaseManager
 import tkinter as tk
 from interfaz.biblioteca_app import BibliotecaApp
-<<<<<<< HEAD
-=======
-from models.libro import Libro
->>>>>>> 5c27baf9bdfb6d0146c133e5cee4fdcb977476a6
+
 def main():
 
     db_manager = DatabaseManager()
